70ec63f0bf5f6eee03af0f0cbc76d6cc53efec39
ElHadji Omar DIONE <omardione1991@gmail.com>
2022-10-10 22:09:01 -0400

    initial commit

---
+++ src/main/java/com/simba/almarjah/AlmarjahApplication.java
@@ -0,0 +1,13 @@
+package com.simba.almarjah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmarjahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmarjahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simba/almarjah/AlmarjahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simba.almarjah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmarjahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
